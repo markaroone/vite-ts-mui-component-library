5adf3ea55947d7039834f4ac917fffd7d6138b85
Mark Christian Fajardo <cfajardo25@gmail.com>
2023-10-02 13:57:07 +0800

    Remove installation of fontsource fonts.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,2 @@
-import '@fontsource-variable/open-sans';
-import '@fontsource/roboto/100-italic.css';
-import '@fontsource/roboto/300-italic.css';
-import '@fontsource/roboto/400-italic.css';
-import '@fontsource/roboto/500-italic.css';
-import '@fontsource/roboto/700-italic.css';
-import '@fontsource/roboto/900-italic.css';
-
 export * from './components';
 export * from './theme/horizonTheme';
