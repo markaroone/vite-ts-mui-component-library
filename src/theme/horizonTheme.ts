bfb90ff864d93ba79b18bff8849b27092c0ebeaf
Mark Christian Fajardo <cfajardo25@gmail.com>
2023-10-02 13:43:46 +0800

    Move font import to index.ts.

--- src/theme/horizonTheme.ts
+++ src/theme/horizonTheme.ts
@@ -1,11 +1,4 @@
 import { alpha, createTheme, getContrastRatio } from '@mui/material';
-import '@fontsource-variable/open-sans';
-import '@fontsource/roboto/100-italic.css';
-import '@fontsource/roboto/300-italic.css';
-import '@fontsource/roboto/400-italic.css';
-import '@fontsource/roboto/500-italic.css';
-import '@fontsource/roboto/700-italic.css';
-import '@fontsource/roboto/900-italic.css';
 
 declare module '@mui/material/styles' {
   interface Palette {
